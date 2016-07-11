6c3733e7bbdb400434e5b3bec56a26c0ebf6aa20
邱读书 <qiudushu@qiudushudeMacBook-Pro.local>
2016-07-11 21:35:25 +0800

    11111

---
+++ DemoStory/DemoStory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoStory
+//
+//  Created by 邱读书 on 16/7/11.
+//  Copyright © 2016年 邱读书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoStory/DemoStory/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoStory
+//
+//  Created by 邱读书 on 16/7/11.
+//  Copyright © 2016年 邱读书. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
